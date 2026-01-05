8ccba0dd3495b0dba54844ff43089ed8c86d8a6b
Naitian Zhou <naitian@naitian.org>
2026-01-05 17:25:45 -0500

    Add colorscheme, lua lsp

--- init.lua
+++ init.lua
@@ -10,11 +10,42 @@ vim.o.shiftwidth = 2
 vim.g.mapleader = " "
 
 vim.o.termguicolors = true
+vim.o.winborder = "rounded"
 
+vim.o.signcolumn = "yes"
+vim.o.scrolloff = 3
+
+-- Colorscheme
+require("vague").setup()
+vim.cmd("colorscheme vague")
+
+-- Package management
 local package_path = vim.fn.stdpath("config")
-require("mini.deps").setup({ path = { package= package_path} })
+require("mini.deps").setup({ path = { package = package_path } })
+local add = MiniDeps.add
 
-MiniDeps.add({
-source = 'neovim/nvim-lspconfig',
-
+add({
+	source = 'neovim/nvim-lspconfig'
 })
+add({
+	source = 'williamboman/mason.nvim'
+})
+add({
+	source = 'nvim-mini/mini.pick'
+})
+add({
+  source = 'saghen/blink.cmp'
+})
+
+require("mini.pick").setup()
+
+-- LSP config
+require("mason").setup()
+vim.lsp.enable({ "lua_ls", "biome" })
+
+-- Keybindings
+vim.keymap.set('n', '<leader><leader>', ':b#<CR>')
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+vim.keymap.set('n', '<leader>i', ':Pick buffers<CR>')
+vim.keymap.set('n', '<leader>p', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>n', ':noh<CR>')
