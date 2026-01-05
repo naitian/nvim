06db7f78f91a71acf37d6c348a7057f2ead0f8b4
Naitian Zhou <naitian@naitian.org>
2026-01-05 18:28:11 -0500

    More settings; completion

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@
 
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.swapfile = false
+vim.o.smartindent = true
+vim.o.undofile = true
 
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
@@ -16,36 +19,80 @@ vim.o.signcolumn = "yes"
 vim.o.scrolloff = 3
 
 -- Colorscheme
-require("vague").setup()
-vim.cmd("colorscheme vague")
+require("kanagawa").setup({
+	colors = {
+		theme = {
+			all = {
+				ui = {
+					bg_gutter = "none"
+				}
+			}
+		}
+	}
+})
+vim.cmd("colorscheme kanagawa")
 
 -- Package management
 local package_path = vim.fn.stdpath("config")
 require("mini.deps").setup({ path = { package = package_path } })
 local add = MiniDeps.add
 
-add({
-	source = 'neovim/nvim-lspconfig'
-})
-add({
-	source = 'williamboman/mason.nvim'
-})
-add({
-	source = 'nvim-mini/mini.pick'
-})
-add({
-  source = 'saghen/blink.cmp'
-})
+add({ source = 'neovim/nvim-lspconfig' })
+add({ source = 'williamboman/mason.nvim' })
+add({ source = 'nvim-mini/mini.pick' })
+add({ source = 'saghen/blink.cmp', checkout = "v1.8.0" })
 
 require("mini.pick").setup()
+require("blink.cmp").setup({
+	keymap = { preset = 'enter' },
+
+	-- (Default) Only show the documentation popup when manually triggered
+	completion = { documentation = { auto_show = false } },
+
+	-- Default list of enabled providers defined so that you can extend it
+	-- elsewhere in your config, without redefining it, due to `opts_extend`
+	sources = {
+		default = { 'lsp', 'path', 'snippets', 'buffer' },
+	},
+
+	-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+	-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+	-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+	--
+	-- See the fuzzy documentation for more information
+	fuzzy = { implementation = "prefer_rust_with_warning" }
+})
 
 -- LSP config
 require("mason").setup()
-vim.lsp.enable({ "lua_ls", "biome" })
+vim.lsp.config("ruff", {
+	init_options = {
+		settings = {
+			lint = {
+				enable = false
+			}
+		}
+	}
+})
+vim.lsp.enable({ "lua_ls", "biome", "ruff", "basedpyright" })
 
 -- Keybindings
-vim.keymap.set('n', '<leader><leader>', ':b#<CR>')
-vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
-vim.keymap.set('n', '<leader>i', ':Pick buffers<CR>')
-vim.keymap.set('n', '<leader>p', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>n', ':noh<CR>')
+local map = vim.keymap.set
+map('n', '<leader><leader>', ':b#<CR>')
+map('n', '<leader>f', vim.lsp.buf.format)
+map('n', '<leader>.', function() return vim.lsp.buf.code_action({}) end)
+map('n', '<leader>o',
+	function()
+		return vim.lsp.buf.code_action({
+			context = { diagnostics = {}, only = { "source.organizeImports" } },
+			apply = true
+		})
+	end
+)
+map('n', '<leader>h', ':Pick help<CR>')
+map('n', '<leader>i', ':Pick buffers<CR>')
+map('n', '<leader>p', ':Pick files<CR>')
+map('n', '<leader>n', ':noh<CR>')
+
+-- Diagnostic
+vim.diagnostic.config({ virtual_text = true })
